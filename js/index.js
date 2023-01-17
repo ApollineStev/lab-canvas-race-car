a32d8e2524e0d88c98b285899d585ed40c802029
ApollineStev <apollinecanard@gmail.com>
2023-01-17 11:18:28 +0100

    .

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
-};
+  function startGame() {
+    const canvas = document.querySelector('#canvas')
+    const ctx = canvas.getContext('2d')
+
+    let road = new Image()
+    road.src = "/images/road.png"
+    let car = new Image()
+    car.src = "/images/car.png"
+
+    road.onload = function() {
+      ctx.drawImage(road, 0, 0, 500, 700)
+    }
+    car.onload = function() {
+      ctx.drawImage(car,220,550,60,100)
+    }
+
+  }
+
+}   
+  
+    
+
+  
+
